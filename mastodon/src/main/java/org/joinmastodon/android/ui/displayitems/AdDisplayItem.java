42ea828fa164dfa8789f4406a49772b13c0b7963
ngobyalincolnisaac <ngobyalincolnisaac@gmail.com>
2026-01-30 23:45:39 +0300

    Update AdDisplayItem.java
    New

--- mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/AdDisplayItem.java
+++ mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/AdDisplayItem.java
@@ -1,7 +1,21 @@
 package org.joinmastodon.android.ui.displayitems;
 
+import org.joinmastodon.android.ui.adapters.GenericListItemsAdapter;
+import org.joinmastodon.android.model.Status;
+
+/**
+ * Represents an Ad item in the Mastodon timeline.
+ */
 public class AdDisplayItem extends StatusDisplayItem {
+
     public AdDisplayItem() {
-        super(null, 0); // no actual status needed
+        // Pass a dummy Status object, because StatusDisplayItem requires one
+        super(new Status(), 0); 
+    }
+
+    @Override
+    public int getType() {
+        // Use a unique type value that your adapter can check
+        return GenericListItemsAdapter.AD_ITEM_VIEW_TYPE;
     }
 }
