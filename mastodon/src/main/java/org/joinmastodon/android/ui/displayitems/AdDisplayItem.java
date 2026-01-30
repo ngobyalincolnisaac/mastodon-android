183749b3eed2c458c2c3f33fdf72b2f07d21663d
ngobyalincolnisaac <ngobyalincolnisaac@gmail.com>
2026-01-30 22:22:07 +0300

    Create AdDisplayItem.java
    Ekiriwo AdDisplayItem

---
+++ mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/AdDisplayItem.java
@@ -0,0 +1,7 @@
+package org.joinmastodon.android.ui.displayitems;
+
+public class AdDisplayItem extends StatusDisplayItem {
+    public AdDisplayItem() {
+        super(null, 0); // no actual status needed
+    }
+}
